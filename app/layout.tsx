2830533a81524d2db5c3928bb1e14a3e9a827d8a
sershishkov <ser.shishkov@gmail.com>
2024-12-31 15:22:43 +0200

    @/app/globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import './globals.css';
+import '@/app/globals.css';
 
 import AuthProvider from '@/components/AuthProvider/AuthProvider';
 import ToastProvider from '@/components/ToastProvider/toast.provider';
